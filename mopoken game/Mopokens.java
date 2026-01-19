e008eaffd0162a31a8c61e9953fbcda5b8001545
Tanushree <tanushree.quest1.io>
2026-01-19 10:30:05 +0530

    added getXP and setXP to Mopokens

--- mopoken game/Mopokens.java
+++ mopoken game/Mopokens.java
@@ -12,14 +12,25 @@ class Mopokens{
     public int getLevel(){
         return level;
     }
+
+    public int getXP(int XP){
+        return XP;
+    }
+
     public void setType(String type){
         this.type = type;
     }
+
     public void setLevel(int level){
         this.level = level;
     }   
+
+    public void setXP(int XP){
+        this.XP = XP;
+    }
     @Override
     public String toString(){
         return type + "#" + level;
     }
+
 }
